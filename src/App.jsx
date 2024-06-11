d19372c524387c0d09862d3e60dc19d74b3e40d5
Chandra Wijaya Sentosa <chandra.w.sentosa@gmail.com>
2024-06-11 14:40:10 +0700

    Install tailwindcss and daisyUI

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import logo from './assets/favicon.svg';
 
 function App() {
   return (
-    <div class="App">
-      <header class="header">
+    <div class="text-center">
+      <header class="flex flex-col items-center justify-center bg-[#282c34] min-h-[100vh] text-white text-xl">
         <img src={logo} class="logo" alt="logo" />
-        <p>
+        <p class="m-2 mt-10">
           Edit <code>src/App.jsx</code> and save to reload.
         </p>
         <a
-          class="link"
+          class="text-[#b318f0]"
           href="https://github.com/solidjs/solid"
           target="_blank"
           rel="noopener noreferrer"

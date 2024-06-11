5eacf5066d870f970d64cb943e8caf1d1dbeec1a
Chandra Wijaya Sentosa <chandra.w.sentosa@gmail.com>
2024-06-11 14:01:39 +0700

    Solidjs js template

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import logo from './assets/favicon.svg';
+
+function App() {
+  return (
+    <div class="App">
+      <header class="header">
+        <img src={logo} class="logo" alt="logo" />
+        <p>
+          Edit <code>src/App.jsx</code> and save to reload.
+        </p>
+        <a
+          class="link"
+          href="https://github.com/solidjs/solid"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Solid
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;

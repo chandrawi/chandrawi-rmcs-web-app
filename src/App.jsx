478fc16d38d090acdce961b7288b0b7b921108de
Chandra Wijaya Sentosa <chandra.w.sentosa@gmail.com>
2024-06-12 20:36:28 +0700

    Install and set up solidjs router

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,15 @@
-import logo from './assets/favicon.svg';
+import { Router, Route } from "@solidjs/router";
+import Home from "./components/Home";
+import Counter from "./components/Counter";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
-    <div class="text-center">
-      <header class="flex flex-col items-center justify-center bg-[#282c34] min-h-[100vh] text-white text-xl">
-        <img src={logo} class="logo" alt="logo" />
-        <p class="m-2 mt-10">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          class="text-[#b318f0]"
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route path="/" component={Home} />
+      <Route path="/counter" component={Counter} />
+      <Route path="/*404" component={NotFound} />
+    </Router>
   );
 }
 

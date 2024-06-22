89f23652a501925b7ad65ab817a58fe4815f40ac
Chandra Wijaya Sentosa <chandra.w.sentosa@gmail.com>
2024-06-23 00:23:42 +0700

    Add menus in dashboard page navbar

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,11 +1,13 @@
-import NavbarDashboard from "./navigation/NavbarDashboard"
+import SvgDashboard from "./svg/SvgDashboard";
+import NavbarDashboard from "./navigation/NavbarDashboard";
 
 export default function Dashboard() {
   return (
     <>
-    <div class="w-[100vw] h-[100vh] bg-gray-500 opacity-30 fixed z-10"></div>
+    <SvgDashboard />
+    <div class="w-[100vw] h-[100vh] bg-gray-500 opacity-30 hidden fixed z-10"></div>
     <NavbarDashboard />
-    <div class="w-full min-h-[calc(100vh-3.5rem)] mt-[3.5rem] bg-slate-50 dark:bg-slate-800">
+    <div class="w-full min-h-[100vh] bg-slate-50 dark:bg-slate-800">
 
         <div class="fixed left-0 z-20 min-w-[250px] w-[20%] bg-white dark:bg-slate-900 text-gray-800 dark:text-gray-200 border-r border-slate-100 dark:border-slate-800 transition-transform md:translate-x-0">
 
--- src/components/navigation/NavbarDashboard.jsx
+++ src/components/navigation/NavbarDashboard.jsx
@@ -1,26 +1,36 @@
+import { Show, createSignal } from "solid-js";
+import TitleMenuDashboard from "./TitleMenuDashboard";
+import SearchMenu from "./SearchMenu";
+import SettingMenu from "./SettingMenu";
+import ProfileMenu from "./ProfileMenu";
 
 export default function NavbarDashboard() {
+
+  let [searchExpand, setSearchExpand] = createSignal(false);
+
   return (
-    <>
-    <div class="fixed z-30 top-0 w-full h-[3.5rem] flex flex-row bg-white dark:bg-slate-900 shadow-md_res shadow-slate-200 dark:shadow-slate-1000 text-gray-800 dark:text-gray-200">
+    <div class="fixed z-30 top-0 w-full h-[3.5rem] flex flex-row bg-white dark:bg-slate-900 shadow-md_res shadow-slate-200 dark:shadow-slate-950 text-gray-800 dark:text-gray-200">
       <div class="md:min-w-[250px] md:w-[20%] w-auto h-full flex flex-row justify-center px-2 xs:px-3 md:px-4">
         <button class="md:hidden h-full flex items-center mr-1.5">
-          <span class="material-icons-outlined text-[2rem]">menu</span>
+          <span class="icon-menu_list text-xl"></span>
         </button>
         <div class="h-full flex flex-row items-center">
-          <img src="/image/gundala_logo.svg" alt="" class="w-[2.25rem] h-8 min-w-[2.25rem] min-h-8 xs:w-[2.5rem] xs:h-[2.25rem] xs:inline" />
-          <img src="/image/gundala_letter.svg" alt="" class="w-[6.875rem] h-[0.75rem] ml-1 hidden md:inline" />
+          <img src="/image/gundala_logo.svg" alt="" class="w-9 h-8 min-w-9 min-h-8 xs:w-10 xs:h-9 xs:inline" />
+          <img src="/image/gundala_letter.svg" alt="" class="w-28 h-3 ml-1 hidden md:inline" />
         </div>
       </div>
-      <div class="grow h-full flex flex-row justify-between mx-0 md:mx-2">
-        <div class="grow h-full">
-
+      <div class="grow h-full flex flex-row justify-between">
+        <div class="grow h-full flex flex-row justify-center md:justify-start">
+          <Show when={!searchExpand()}>
+            <TitleMenuDashboard />
+          </Show>
         </div>
         <div class="h-full flex flex-row">
-
+          <SearchMenu searchExpand={searchExpand} setSearchExpand={setSearchExpand} />
+          <SettingMenu login={true} />
+          <ProfileMenu login={true} />
         </div>
       </div>
     </div>
-    </>
   );
 };
